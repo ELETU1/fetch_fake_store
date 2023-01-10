61d657d74c1ccd34613dfaceb0dd062a16e60b85
ELETUTECH <Issaissaeletu@gmail.com>
2023-01-10 00:58:10 -0800

    version 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
 
-function App() {
+const App = () => {
+  const[data, setData] = useState([])
+
+  useEffect( ()=> {
+      fakeStore(data)
+  }, [])
+  
+  const fakeStore = async ()=>{
+    const res = await fetch("https://fakestoreapi.com/products")
+    const jsonData = await res.json()
+    setData(jsonData)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h2>Fake API Store</h2>
+      <div className="container">
+        {data.map( (d) =>{
+          return(
+            <div className="box">
+              <div className="content">
+                <h5>{d.title}</h5>
+                <p>{d.description}</p>
+              </div>
+              <img src={d.image} alt="" />
+           </div>  
+          )
+        })}   
+      </div>
+    </>
+  )
 }
 
 export default App;
